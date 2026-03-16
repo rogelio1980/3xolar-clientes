662979ce1120feb80e12bb4e727e365d5322c24c
Rogelio Guerra <rogelioguerra@MacBook-Pro-de-Rogelio-2.local>
2026-03-16 11:04:56 -0600

    feat: portal completo con cotizador, admin, viewer y chat

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAk0kJlMkFyUbZYp-v-xv3t0DrQsE433s0",
+  authDomain: "xolar-clientes.firebaseapp.com",
+  projectId: "xolar-clientes",
+  storageBucket: "xolar-clientes.firebasestorage.app",
+  messagingSenderId: "361581457372",
+  appId: "1:361581457372:web:7a7f855281f87793a5e919"
+};
